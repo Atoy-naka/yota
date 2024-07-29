672d31623eb775a15cedcfb43ec94e3f9a6d932c
Atoy-naka <nakamura_1009@outlook.jp>
2024-07-29 21:05:46 +0900

    ページネーション実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,22 +3,21 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
+        
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>{{ $posts->links()}}</div>
-        <h2 class='title'>
-            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-        </h2>
+        <div class='paginate'>{{ $posts->links() }}</div>
     </body>
 </html>
